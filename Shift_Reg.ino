510740d27dc72065365213025c5deee264233c2d
Orens Xhagolli <orens.xhagolli@hotmail.com>
2016-08-10 15:04:27 -0400

    Fixed files

--- Shift_Reg.ino
+++ Shift_Reg.ino
@@ -11,7 +11,7 @@ int BIN2 = 3;
 //Store State
 int aState, bState;
 
-//How many of the shift registers - change this
+//How many of the shift registers
 #define number_of_74hc595s 2
 
 //do not touch
@@ -20,21 +20,21 @@ int aState, bState;
 boolean registers[numOfRegisterPins];
 
 void shiftSetup() {
-  pinMode(SER_Pin, OUTPUT);
-  pinMode(RCLK_Pin, OUTPUT);
-  pinMode(SRCLK_Pin, OUTPUT);
+ pinMode(SER_Pin, OUTPUT);
+ pinMode(RCLK_Pin, OUTPUT);
+ pinMode(SRCLK_Pin, OUTPUT);
 
-  //reset all register pins
-  clearRegisters();
-  writeRegisters();
+ //reset all register pins
+ clearRegisters();
+ writeRegisters();
 }
 
 
 //set all register pins to LOW
 void clearRegisters() {
-  for (int i = numOfRegisterPins - 1; i >=  0; i--) {
-    registers[i] = LOW;
-  }
+ for (int i = numOfRegisterPins - 1; i >=  0; i--) {
+   registers[i] = LOW;
+ }
 }
 
 
@@ -42,108 +42,106 @@ void clearRegisters() {
 //Only call AFTER all values are set how you would like (slow otherwise)
 void writeRegisters() {
 
-  digitalWrite(RCLK_Pin, LOW);
+ digitalWrite(RCLK_Pin, LOW);
 
-  for (int i = numOfRegisterPins - 1; i >=  0; i--) {
-    digitalWrite(SRCLK_Pin, LOW);
+ for (int i = numOfRegisterPins - 1; i >=  0; i--) {
+   digitalWrite(SRCLK_Pin, LOW);
 
-    int val = registers[i];
+   int val = registers[i];
 
-    digitalWrite(SER_Pin, val);
-    digitalWrite(SRCLK_Pin, HIGH);
+   digitalWrite(SER_Pin, val);
+   digitalWrite(SRCLK_Pin, HIGH);
 
-  }
-  digitalWrite(RCLK_Pin, HIGH);
+ }
+ digitalWrite(RCLK_Pin, HIGH);
 
 }
 
 //set an individual pin HIGH or LOW
 void setRegisterPin(int index, int value) {
-  registers[index] = value;
+ registers[index] = value;
 }
 
 void motorDirection(int motorAout, int motorBout) {
 
-  int statechange;
+ int statechange;
 
-  if (aState != motorAout) {
-    statechange = 1;
+ if (aState != motorAout) {
+   statechange = 1;
 
-    if (motorAout == 2) {
-      setRegisterPin(0, HIGH);
-      setRegisterPin(1, LOW);
+   if (motorAout == 2) {
+     setRegisterPin(0, HIGH);
+     setRegisterPin(1, LOW);
 
-    } else if (motorAout == 1) {
-      setRegisterPin(0, LOW);
-      setRegisterPin(1, HIGH);
+   } else if (motorAout == 1) {
+     setRegisterPin(0, LOW);
+     setRegisterPin(1, HIGH);
 
-    } else {
-      setRegisterPin(0, LOW);
-      setRegisterPin(1, LOW);
-    }
-  }
-  if (bState != motorBout) {
-    statechange = 1;
-    if (motorBout == 2) {
-      setRegisterPin(2, HIGH);
-      setRegisterPin(3, LOW);
+   } else {
+     setRegisterPin(0, LOW);
+     setRegisterPin(1, LOW);
+   }
+ }
+ if (bState != motorBout) {
+   statechange = 1;
+   if (motorBout == 2) {
+     setRegisterPin(2, HIGH);
+     setRegisterPin(3, LOW);
 
-    } else if (motorBout == 1) {
-      setRegisterPin(2, LOW);
-      setRegisterPin(3, HIGH);
-    } else {
-      setRegisterPin(2, LOW);
-      setRegisterPin(3, LOW);
-    }
-  }
-  //If states are changed update ouputs! Takes ~8ms
-  if (statechange == 1) {
-    writeRegisters();
-    aState = motorAout;
-    bState = motorBout;
-  }
+   } else if (motorBout == 1) {
+     setRegisterPin(2, LOW);
+     setRegisterPin(3, HIGH);
+   } else {
+     setRegisterPin(2, LOW);
+     setRegisterPin(3, LOW);
+   }
+ }
+ //If states are changed update ouputs! Takes ~8ms
+ if (statechange == 1) {
+   writeRegisters();
+   aState = motorAout;
+   bState = motorBout;
+ }
 }
 
 void motorDirectionBackup(int motorAout, int motorBout) {
 
-  int statechange;
+ int statechange;
 
-  if (aState != motorAout) {
-    statechange = 1;
+ if (aState != motorAout) {
+   statechange = 1;
 
-    if (motorAout == 2) {
-      setRegisterPin(0, HIGH);
-      setRegisterPin(1, LOW);
+   if (motorAout == 2) {
+     setRegisterPin(0, HIGH);
+     setRegisterPin(1, LOW);
 
-    } else if (motorAout == 1) {
-      setRegisterPin(0, LOW);
-      setRegisterPin(1, HIGH);
+   } else if (motorAout == 1) {
+     setRegisterPin(0, LOW);
+     setRegisterPin(1, HIGH);
 
-    } else {
-      setRegisterPin(0, LOW);
-      setRegisterPin(1, LOW);
-    }
-  }
-  if (bState != motorBout) {
-    statechange = 1;
-    if (motorBout == 2) {
-      setRegisterPin(2, HIGH);
-      setRegisterPin(3, LOW);
+   } else {
+     setRegisterPin(0, LOW);
+     setRegisterPin(1, LOW);
+   }
+ }
+ if (bState != motorBout) {
+   statechange = 1;
+   if (motorBout == 2) {
+     setRegisterPin(2, HIGH);
+     setRegisterPin(3, LOW);
 
-    } else if (motorBout == 1) {
-      setRegisterPin(2, LOW);
-      setRegisterPin(3, HIGH);
-    } else {
-      setRegisterPin(2, LOW);
-      setRegisterPin(3, LOW);
-    }
-  }
-  //If states are changed update ouputs! Takes ~8ms
-  if (statechange == 1) {
-    writeRegisters();
-    aState = motorAout;
-    bState = motorBout;
-  }
+   } else if (motorBout == 1) {
+     setRegisterPin(2, LOW);
+     setRegisterPin(3, HIGH);
+   } else {
+     setRegisterPin(2, LOW);
+     setRegisterPin(3, LOW);
+   }
+ }
+ //If states are changed update ouputs! Takes ~8ms
+ if (statechange == 1) {
+   writeRegisters();
+   aState = motorAout;
+   bState = motorBout;
+ }
 }
-
-
